e15b0cde6a122c66064e0bf0663c0bfbbd100174
OlgaS <olgaselesnyova@gmail.com>
2024-02-27 14:44:33 +0200

    visual added

--- ollamaGradio2.py
+++ ollamaGradio2.py
@@ -11,7 +11,7 @@ def get_language(country, file):
     return data[country]
 
 
-def examples(file):
+def get_examples(file):
     with open(file, 'r') as f:
         data = f.read()
     return data
@@ -24,48 +24,59 @@ def transcribe_audio(audio_file):
     result = model.transcribe(audio_tensor, fp16=False)['text']
     return result
 
-#CHECK THAT AUDIO INPUT EXISTS
+
+def match_task(text, audio):
+    if text and audio:
+        return text
+    elif text:
+        return text
+    elif audio:
+        return transcribe_audio(audio)
+    else:
+        return ReferenceError("No input provided.")
+    
+
 def llm(task, country, number):
     llm = Ollama(model="mistral")
+    
     lang = get_language(country, file="country_to_language.json")
-    few_shot = examples(file="fewshot_learning.txt")
+    few_shot = get_examples(file="fewshot_learning.txt")
     context = f"You are a helpful assistant. You give an enumerated list of phrases. You answer concisely and only in {lang}."
     icl = f"For example, {few_shot}"
     query = f"I'm travelling to {country}. Which {number} most popular phrases should I learn to {task}?"
     phrases = llm.invoke(context+icl+query)    
     return phrases
     
+
 def main():    
     demo = gr.Blocks()
-    with gr.Blocks() as demo:
+    with gr.Blocks(theme=gr.themes.Soft()) as demo:
+        # with gr.Column():
         gr.Markdown("# Assistant for Travelers")
-        # gr.Markdown("Add text or audio to get the most popular phrases for your travel destination.")
+        gr.Markdown("### What do you want to do: order food in the restaurant, ask for direction, buy tickets?")
+        gr.Markdown("### Record audio or enter text.")
         with gr.Row():
             with gr.Column():
-                gr.Markdown("## Inputs: ")
-                with gr.Row():
-                    # gr.Markdown("### Text input: ")
-                    # gr.Markdown("### Audio input: ")
-                    # gr.Markdown("## Inputs: ")
-                        # input_type = gr.Radio(["Text", "Audio"], label="Input type", info="Choose the input type")
-                    with gr.Column():
-                        gr.Markdown("### Text input: ")
-                        task = gr.Textbox(info="What do you want to do?", label="Task", placeholder="order food, ask for directions, etc.")
-                    with gr.Column():
-                        gr.Markdown("### Audio input: ")
-                        audio = gr.Audio(sources=["microphone"], label="Record your voice", type="filepath", max_length=10) 
-                        stt = gr.Button("Transcribe audio")
-                        task = gr.Textbox("Transcribed text will appear here.")
-                        stt.click(fn=transcribe_audio, inputs=audio, outputs=task) 
-                    with gr.Column():
-                        country = gr.Radio(["France", "Germany", "Italy"], label="Location", info="Where are you travelling?")
-                        num = gr.Slider(0, 10, value=5, step=1, info="How many phrases?", label="Number of phrases")
+                text = gr.Textbox(label="Enter text", placeholder="order food, ask for directions, etc.")
             with gr.Column():
-                gr.Markdown("## Output: ")
-                response = gr.Button("Generate response")
-                out = gr.Textbox("Response will appear here.")
+                audio = gr.Audio(sources=["microphone"], label="Record your voice", type="filepath", max_length=10) 
+            #     stt = gr.Button("Transcribe audio")
+            # with gr.Column():
+            #     task = gr.Textbox()
+            #     stt.click(fn=transcribe_audio, inputs=audio, outputs=task) 
+        with gr.Row():
+            with gr.Row():
+                country = gr.Radio(["France", "Germany", "Italy"], label="Location", info="Where are you travelling?")
+                num = gr.Slider(0, 10, value=5, step=1, info="How many phrases?", label="Number of phrases")    
+        with gr.Row():
+            with gr.Column():
+                with gr.Row():
+                    response = gr.Button("Generate response", variant="primary")
+                    clear = gr.Button("Clear")
+                out = gr.Textbox(label="Response")
+                task = match_task(text, audio)
                 response.click(fn=llm, inputs=[task, country, num], outputs=out)
-            
+                
     demo.launch(share=False, debug=True)
 
 
