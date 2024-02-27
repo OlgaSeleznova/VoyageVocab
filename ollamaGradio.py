110ead63a734f4497bdc7ed60efca9fedd1eab20
OlgaS <olgaselesnyova@gmail.com>
2024-02-27 16:11:31 +0200

    gradio adding clear button

--- ollamaGradio.py
+++ ollamaGradio.py
@@ -2,21 +2,24 @@ from langchain_community.llms import Ollama
 import gradio as gr
 import json
 import whisper
-import numpy as np
-import torch, torchaudio
+import torch
 
+
+#function to load convert name of the country to the language spoken in that country
 def get_language(country, file):
     with open(file, 'r') as f:
         data = json.load(f)
     return data[country]
 
 
+#function to load examples of phrases for few shot learning
 def get_examples(file):
     with open(file, 'r') as f:
         data = f.read()
     return data
 
 
+#function to transcribe audio to text using whisper
 def transcribe_audio(audio_file):
     model = whisper.load_model("base")
     audio = whisper.load_audio(audio_file,sr=16000)
@@ -25,6 +28,7 @@ def transcribe_audio(audio_file):
     return result
 
 
+#function to match the task to the input provided: audio or text
 def match_task(text, audio):
     if text and audio:
         return text
@@ -34,13 +38,13 @@ def match_task(text, audio):
         return transcribe_audio(audio)
     else:
         return ReferenceError("No input provided.")
-    
 
+
+#function to generate phrases for the user
 def llm(task, country, number):
     llm = Ollama(model="mistral")
-    
-    lang = get_language(country, file="country_to_language.json")
-    few_shot = get_examples(file="fewshot_learning.txt")
+    lang = get_language(country, file="utils/country_to_language.json")
+    few_shot = get_examples(file="utils/fewshot_learning.txt")
     context = f"You are a helpful assistant. You give an enumerated list of phrases. You answer concisely and only in {lang}."
     icl = f"For example, {few_shot}"
     query = f"I'm travelling to {country}. Which {number} most popular phrases should I learn to {task}?"
@@ -48,35 +52,36 @@ def llm(task, country, number):
     return phrases
     
 
+#function to launch the application
 def main():    
     demo = gr.Blocks()
+    #create a gradio interface
     with gr.Blocks(theme=gr.themes.Soft()) as demo:
-        # with gr.Column():
         gr.Markdown("# Assistant for Travelers")
         gr.Markdown("### What do you want to do: order food in the restaurant, ask for direction, buy tickets?")
         gr.Markdown("### Record audio or enter text.")
+        #create a row with two columns
         with gr.Row():
             with gr.Column():
                 text = gr.Textbox(label="Enter text", placeholder="order food, ask for directions, etc.")
             with gr.Column():
                 audio = gr.Audio(sources=["microphone"], label="Record your voice", type="filepath", max_length=10) 
-            #     stt = gr.Button("Transcribe audio")
-            # with gr.Column():
-            #     task = gr.Textbox()
-            #     stt.click(fn=transcribe_audio, inputs=audio, outputs=task) 
+        # create a row with two blocks
         with gr.Row():
-            with gr.Row():
-                country = gr.Radio(["France", "Germany", "Italy"], label="Location", info="Where are you travelling?")
-                num = gr.Slider(0, 10, value=5, step=1, info="How many phrases?", label="Number of phrases")    
+            country = gr.Radio(["France", "Germany", "Italy"], label="Location", info="Where are you travelling?")
+            num = gr.Slider(0, 10, value=5, step=1, info="How many phrases?", label="Number of phrases")    
+        #create a row with two buttons
         with gr.Row():
             with gr.Column():
-                with gr.Row():
-                    response = gr.Button("Generate response", variant="primary")
-                    clear = gr.Button("Clear")
-                out = gr.Textbox(label="Response")
-                task = match_task(text, audio)
-                response.click(fn=llm, inputs=[task, country, num], outputs=out)
-                
+                response = gr.Button("Generate response", variant="primary")
+            with gr.Column():
+                clear = gr.Button("Clear")
+        #create a row for response
+        with gr.Row():
+            out = gr.Textbox(label="Response")
+            task = match_task(text, audio)
+        response.click(fn=llm, inputs=[task, country, num], outputs=out) 
+        gr.ClearButton.add(clear, [text, audio, country, num, out])   
     demo.launch(share=False, debug=True)
 
 
