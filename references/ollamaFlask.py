0b0613de724883092c90618750a1bb12bb548ce8
OlgaS <olgaselesnyova@gmail.com>
2024-02-29 14:30:43 +0200

    flask

--- references/ollamaFlask.py
+++ references/ollamaFlask.py
@@ -1,11 +1,18 @@
-from flask import Flask, request, jsonify
+from langchain.prompts.pipeline import PipelinePromptTemplate
+from langchain.prompts.prompt import PromptTemplate
 from langchain_community.llms import Ollama
-
+import argparse
+from flask import Flask, request, jsonify
+from langchain_core.prompts import ChatPromptTemplate
+import json
 
 app = Flask(__name__)
-@app.route('/ollm')
+@app.route('/chain', methods=['POST'])
 def ollm():
-    llm = Ollama(model="llama2:13b-chat")
-    input = request.args.get('query')
-    answer = llm.invoke(f"I'm travelling to {input.country}. Which phrases should I learn to {input.task}?")
-    return answer
+    if request.method == 'POST':
+        llm = Ollama(model="llama2:13b-chat")
+        input = json.loads(request.data)
+        answer = llm.invoke(f"You are a foreign language tutor. I'm travelling to {input['country']}. 
+                            Which {input["phrases_num"]} phrases should I learn to {input['task']}?")
+        return answer
+    
